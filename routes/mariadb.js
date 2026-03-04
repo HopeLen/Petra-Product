4394c31776efdf2d2ae904d5ed9bd5fdca06074c
Hope <roni.trololo2004@gmail.com>
2026-03-04 12:07:20 +0200

    Fixed database login for testing

--- routes/mariadb.js
+++ routes/mariadb.js
@@ -2,9 +2,9 @@ const mariadb = require("mariadb");
 const pool = mariadb.createPool({
   host: process.env.DB_HOST, // or your server IP
   port: "3306",
-  user: process.env.DB_USER, // MariaDB username
-  password: process.env.DB_PASSWORD, // MariaDB password
-  database: process.env.DB_NAME, // Your database name
+  user: process.env.DB_USER || "root", // MariaDB username
+  password: process.env.DB_PASSWORD || "root", // MariaDB password
+  database: process.env.DB_NAME || "petra_test", // Your database name
   connectionLimit: 5,
   charset: "utf8mb4", // important for Hebrew support
 });
